a80db5b45fe7fbd1578d25f2eb73d9c759fadfd8
Leeboy93 <83501520+Leeboy93@users.noreply.github.com>
2023-10-01 09:04:06 +0100

    Update milestone_2

--- milestone_2.py
+++ milestone_2.py
@@ -1,8 +1,5 @@
 import random
 
-word_list = ["Apple", "Pear", "Orange", "Banana", "Grape"]
-print(word_list)
-
 def word():
     word_list = ["Apple", "Pear", "Orange", "Banana", "Grape"]
     return random.choice(word_list)
